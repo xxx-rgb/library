a26cf3e3ae6d1edfcf5935ba15be0c4d4e9a1d7a
Кофанов Антон Алексеевич <ANTKOF@avalon.ru>
2021-05-10 19:57:27 +0300

    new reviews file

---
+++ reviews.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>hello</title>
+</head>
+<body>
+<form method="get" action="1.php">
+    <input type="text" name="new" placeholder="Зарегистрироватьтся">
+    <input type="text" name="old" placeholder="Войти">
+    <input type="submit" value="Отправить">
+</form>
+</body>
+</html>
+
+
